72039625e40891505fcd56be061df621727ce5ce
zj <zhaozj1225@outlook.com>
2021-11-11 15:36:08 +0800

    完成16.1.8

--- chapter_16/sitka_highs_lows.py
+++ chapter_16/sitka_highs_lows.py
@@ -3,26 +3,29 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-filename = r'data\sitka_weather_07-2018_simple.csv'
+filename = r'data\sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     # get the max temperature
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        low = int(row[6])
         dates.append(date)
         highs.append(high)
+        lows.append(low)
 
 # draw the chart according to high temperature
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='b')
 
 # set the format
-ax.set_title('the high daily temperature of July, 2018', fontsize=24)
+ax.set_title('the daily temperature of 2018', fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('temperature(F)', fontsize=16)
