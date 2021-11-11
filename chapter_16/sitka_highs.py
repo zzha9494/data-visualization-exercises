516b5b4de72e0d7e37f132b91fe439253be671e1
zj <zhaozj1225@outlook.com>
2021-11-11 15:27:11 +0800

    完成16.1.6

--- chapter_16/sitka_highs.py
+++ chapter_16/sitka_highs.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 
@@ -8,19 +9,22 @@ with open(filename) as f:
     header_row = next(reader)
 
     # get the max temperature
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(date)
         highs.append(high)
 
 # draw the chart according to high temperature
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 # set the format
 ax.set_title('the high daily temperature of July, 2018', fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel('temperature(F)', fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=16)
 
