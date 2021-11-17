ce091ced5e03782e211b8fad0cfd276273a365df
zj <zhaozj1225@outlook.com>
2021-11-17 16:43:27 +0800

    完成17.2.1

---
+++ chapter_17/python_repos_visual.py
@@ -0,0 +1,48 @@
+import requests
+from plotly.graph_objs import Bar
+from plotly import offline
+
+# call API and store the response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f'Status code: {r.status_code}')
+
+# turn the response into a dict
+response_dict = r.json()
+repo_dicts = response_dict['items']
+
+repo_names, stars = [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# visualize
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6
+}]
+
+my_layout = {
+    'title': 'the most popular projects on github'.title(),
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14}
+    },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14}
+    }
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
